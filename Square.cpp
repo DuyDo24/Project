28a18dea04fd8191eee350e9548e2a7682bd8d88
dipperz462 <dipperz@C255CD9414TDX.curric.gihs.sa.edu.au>
2024-10-10 15:35:40 +1030

    fixed square

--- Square.cpp
+++ Square.cpp
@@ -1,13 +1,13 @@
 #include "Square.h"
 
 Square::Square() {
-    square.setFillColor(sf::Color::Blue);
+    square.setFillColor(sf::Color(122,122,122,255));
     square.setSize(sf::Vector2f(50,50));
     square.setPosition(sf::Vector2f(100,100));
 }
 
 void Square::setColor(sf::Color color) {
-    square.setFillColor(sf::Color::White);
+    square.setFillColor(color);
 }
 
 void Square::draw(sf::Vector2f& position, sf::RenderWindow& window) {
