a3e25031840f58f12f4df4a551c68e2f9e04b6b0
RhysDavies07 <rldavies2007@gmail.com>
2024-10-14 18:11:42 +1030

    comments

--- Player.cpp
+++ Player.cpp
@@ -7,12 +7,12 @@
 Player::Player(int color) {
     this->color = color; 
     for(int i=0;i<3;i++) {
-        hand.push_back(nullptr);
+        hand.push_back(nullptr); //creating null ptrs for cards
     }
 }
 
 void Player::generateCards(std::vector<Piece*> pieces, sf::Font &font) {
-    int visSize = pieces.size();
+    int visSize = pieces.size(); //finding size of entire pieces
     std::vector<Piece*> availPieces;
     int availSize;
     bool found = false;
@@ -25,7 +25,7 @@ void Player::generateCards(std::vector<Piece*> pieces, sf::Font &font) {
             }
         }
         if(!found) {
-            availPieces.push_back(pieces[i]);
+            availPieces.push_back(pieces[i]); //this logic only adds types of pieces
         }
     }
     int size = availPieces.size();
@@ -34,9 +34,9 @@ void Player::generateCards(std::vector<Piece*> pieces, sf::Font &font) {
     
     for(int i=0;i<3;i++) {
         if (hand[i] != nullptr) {
-            delete hand[i];  // Free the old Card object to avoid memory leaks
+            delete hand[i];  // Free the old Card objects to avoid memory leaks
         }
-        num = rand() % size;
+        num = rand() % size; //generating a random card from available cards
         hand[i] = new Card(availPieces[num], font, i);
     }
 }
