0a0ecef29f0cd70008a2e2a080dcefda4b091369
RhysDavies07 <rldavies2007@gmail.com>
2024-10-18 13:42:07 +1030

    finished

---
+++ CardTest.h
@@ -0,0 +1,58 @@
+#include <iostream>
+#include "Card.h"
+#include "Piece.h"  // Assuming this is defined elsewhere
+#include "Pawn.h"
+#include "Board.h"
+#include "Bishop.h"
+
+class CardUnitTest {
+public:
+    void runTests() {
+        testCardDefaultConstructor();
+        testCardParameterizedConstructor();
+        testSetHighlight();
+    }
+    Board board;
+
+private:
+    void testCardDefaultConstructor() {
+        Card card;
+
+        if (card.getPiece() != nullptr) {
+            std::cout << "Default Constructor Test failed!" << std::endl;
+        } else {
+            std::cout << "Default Constructor Test passed!" << std::endl;
+        }
+    }
+
+    void testCardParameterizedConstructor() {
+        Pawn pawn(board.getChessSquare(7, 1), true);  // Assuming Piece has a default constructor
+        sf::Font font;
+        font.loadFromFile("arial.ttf.ttf");  // Make sure to have a valid path
+
+        Card card(&pawn, font, 0);
+
+        if (card.getPiece() != &pawn) {
+            std::cout << "Parameterized Constructor Test failed!" << std::endl;
+        } else {
+            std::cout << "Parameterized Constructor Test passed!" << std::endl;
+        }
+    }
+
+    void testSetHighlight() {
+        Bishop bishop(board.getChessSquare(7, 1), false);
+        sf::Font font;
+        font.loadFromFile("arial.ttf");
+        Card card(&bishop, font, 0);
+
+        card.setHighlight(true);
+        // Assuming there is a way to check if the highlight is set, you would add a condition here
+        // If no way to check, just print something for now.
+        sf::Color HighlightColor = sf::Color(255, 122, 122, 255);
+        if(card.getHighlight() == HighlightColor){
+            std::cout << "SetHighlight Test passed" << std::endl;
+        } else {
+            std::cout << "SetHighlight Test failed" << std::endl;
+        }
+    }
+};
